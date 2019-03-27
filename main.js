5f42f856c6a6462c3124af04f6e1bdd333cbdad1
sowluss <sowluss@gmail.com>
2019-03-27 22:43:53 +0000

    modified .css and .js files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
-const auto = false;
+const auto = true;
 const intervalTime = 5000;
 let slideInterval;
 
@@ -11,9 +11,46 @@ const nextSlide = () => {
   // remove current class
   current.classList.remove('current');
   // check for next slide
-  if (current.nectElementSibling) {
+  if (current.nextElementSibling) {
     current.nextElementSibling.classList.add('current');
   } else {
     slides[0].classList.add('current');
   }
+  setTimeout(() => current.classList.remove('current'));
+}
+
+const prevSlide = () => {
+  // get current class
+  const current = document.querySelector('.current');
+  // remove current class
+  current.classList.remove('current');
+  // check for prev slide
+  if (current.previousElementSibling) {
+    current.previousElementSibling.classList.add('current');
+  } else {
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+// button events
+next.addEventListener('click', e => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+prev.addEventListener('click', e => {
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+// auto slide
+
+if (auto) {
+  slideInterval = setInterval(nextSlide, intervalTime);
 }
